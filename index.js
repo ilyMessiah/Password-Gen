a37cee71520beb66a62289f677dc5c4bc7eb6746
ilyMessiah <109890786+ilyMessiah@users.noreply.github.com>
2022-07-24 02:01:32 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function passwordGen() {
+
+    const characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
+      const randomlyGeneratedChars = [];
+      for (let x = 0; x < 11; ++x) {
+        const randomChar = characters.charAt(Math.floor(Math.random() * characters.length));
+        randomlyGeneratedChars.push(randomChar);
+      }
+      return randomlyGeneratedChars.join('');
+    }
+
+    const pwd = passwordGen()
+
+    console.log(pwd)
