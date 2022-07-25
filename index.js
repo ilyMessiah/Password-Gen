9abae3557a5dfbdce7523d0669de444c0d434959
messiah <109890786+ilyMessiah@users.noreply.github.com>
2022-07-25 03:32:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function passwordGen() {
 
     const characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    
       const randomlyGeneratedChars = [];
+    
       for (let x = 0; x < 11; ++x) {
+          
         const randomChar = characters.charAt(Math.floor(Math.random() * characters.length));
+          
         randomlyGeneratedChars.push(randomChar);
       }
       return randomlyGeneratedChars.join('');
